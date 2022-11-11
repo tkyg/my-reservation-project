9ed37590e09220da3e3028e8cfb7f97b4c759aac
tkyg <80114433+tkyg@users.noreply.github.com>
2022-11-11 13:51:48 -0500

    use POST to send data to backend

--- src/components/NewTable.jsx
+++ src/components/NewTable.jsx
@@ -1,21 +1,40 @@
 import React, { useState } from 'react'
+import { Input, Form, InputCreate } from '../styles/formStyle'
+import { headers, baseUrl } from '../Global'
+import { useNavigate } from 'react-router-dom'
+import '../index.css'
+
 
 const NewTable = () => {
   const [ tableNumber, setTableNumber ] = useState('')
+  const navigate = useNavigate()
 
   const handleChange = e => {
     setTableNumber(e.target.value)
   }
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+    const body = { table_number: tableNumber }
+    const options = {
+      method: 'POST',
+      headers,
+      body: JSON.stringify(body)
+    }
+    const response = await fetch(baseUrl + '/tables', options)
+    const data = await response.json()
+    navigate('/tables')
+  }
   return (
-    <div>
+    <div style={{"fontFamily": "Mukta"}}>
       <h1>NewTable</h1>
-      <form>
+      <Form onSubmit={ handleSubmit }>
         <div>
-          <label>Table Number: </label>
-          <input type="number" value={ tableNumber } onChange={ handleChange }/>
+          <label htmlFor="tableNumber">Table Number: </label>
+          <Input type="number" id="tableNumber" value={ tableNumber } onChange={ handleChange } autoFocus={ true }/>
         </div>
-        <input type="submit" value="Create Table" />
-      </form>
+        <InputCreate type="submit" value="Create Table" />
+      </Form>
     </div>
   )
 }
