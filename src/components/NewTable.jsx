d59ee7fcb93922e115b10fe9d4256a62936c06d5
tkyg <80114433+tkyg@users.noreply.github.com>
2022-11-11 13:14:27 -0500

    create new table

--- src/components/NewTable.jsx
+++ src/components/NewTable.jsx
@@ -4,6 +4,13 @@ const NewTable = () => {
   return (
     <div>
       <h1>NewTable</h1>
+      <form>
+        <div>
+          <label>Table Number: </label>
+          <input type="number" />
+        </div>
+        <input type="submit" value="Create Table" />
+      </form>
     </div>
   )
 }
