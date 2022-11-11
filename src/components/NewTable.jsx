60ba7294d48248d43613056aefff4f594a93c8cf
tkyg <80114433+tkyg@users.noreply.github.com>
2022-11-11 13:19:32 -0500

    useState to make form controlled

--- src/components/NewTable.jsx
+++ src/components/NewTable.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const NewTable = () => {
+  const [ tableNumber, setTableNumber ] = useState('')
+
+  const handleChange = e => {
+    setTableNumber(e.target.value)
+  }
   return (
     <div>
       <h1>NewTable</h1>
       <form>
         <div>
           <label>Table Number: </label>
-          <input type="number" />
+          <input type="number" value={ tableNumber } onChange={ handleChange }/>
         </div>
         <input type="submit" value="Create Table" />
       </form>
